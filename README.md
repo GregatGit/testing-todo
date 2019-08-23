a9cbe6033e97454cc74372f81de91b1cef14bc62
gregatgit <gregdd@outlook.com>
2019-08-23 22:12:30 +1000

    at helper functions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Testing Todos
 
-This is my first TDD app. I am testing with Jest and Enzyme. I followed along simple tutorials excellent youtube course on testing in React and this is my first try.
+This is my first TDD app. I am testing with Jest and Enzyme. I followed along simpletut excellent youtube course on testing in React and this is my first try. - https://www.youtube.com/simpletut
 
 Greg Duncan
 
--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -1,22 +1,20 @@
-import React from  'react'
+import React from 'react'
 import { shallow } from 'enzyme'
 
 import TodoList from './TodoList'
 
-const setUp = (props = {}) => {
+const setUp = (props = []) => {
   const component = shallow(<TodoList {...props} />)
   return component
 }
 
-describe('TodoList componet', () => {
-  
-  let component
-  beforeEach(() => {
-    component = setup()
-  })
+describe('TodoList component', () => {
 
-  it('should render with todo', () => {
-    const wrapper = 
+  describe('Checking without props', () => {
+
+    it('should render without props', () => {
+      const componet = setUp()
+      
+    })
   })
-  
 })
