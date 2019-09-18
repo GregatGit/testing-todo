b3b998b498c8595086180aecbef00dee06a5ba52
gregatgit <gregdd@outlook.com>
2019-09-18 13:09:03 +1000

    basic layout of todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <div data-test="todoList">
       Todo List
+      <ul>
+        {todos
+          ? todos.map((todo, index) => {
+              return <li key={index}>{todo.todo}</li>
+            })
+          : null}
+      </ul>
     </div>
   )
 }
 
-export default TodoList
+const mapStateToProps = (state, ownProps) => {
+  return {
+    todos: state.todos,
+  }
+}
+
+export default connect(mapStateToProps)(TodoList)
