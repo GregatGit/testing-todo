3e61a55c2e1e1fafd7251c107df34d8ffa104919
gregatgit <gregdd@outlook.com>
2019-09-21 09:39:18 +1000

    connect addTodo to reducer

--- src/reducers/reducer_todos.js
+++ src/reducers/reducer_todos.js
@@ -1,8 +1,17 @@
+import { ADD_TODO } from '../actions'
+
 const init = [
   { todo: 'Buy Lunch', done: false },
   { todo: 'Clean Shoes', done: false },
 ]
 
 export default function reducer_todos(state = init, action) {
-  return state
+
+  switch (action.type) {
+    case ADD_TODO:
+        return [...state, action.payload]      
+ 
+    default:
+      return state
+  }
 }
