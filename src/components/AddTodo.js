d963d34318307ae9a829af07c6ef8d4380cab1bc
gregatgit <gregdd@outlook.com>
2019-09-21 09:33:09 +1000

    create todo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 const AddTodo = () => {
+  const [todo, setTodo] = useState('')
+
+  const handleChange = e => {
+    setTodo(e.target.value)
+  }
+  const handleAddTodo = () => {
+    console.log(todo)
+  }
   return (
     <div data-test="AddTodo">
       <h2>ADD TASK TO LIST</h2>
@@ -14,7 +22,10 @@ const AddTodo = () => {
         minLength="3"
         maxLength="20"
         size="20"
+        onChange={handleChange}
       />
+      <h3>{todo}</h3>
+      {todo.length > 2 ? <button onClick={handleAddTodo}>ADD TODO</button> : 'must be 3 chars long'}
     </div>
   )
 }
