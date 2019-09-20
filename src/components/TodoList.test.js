615999bd390842b7067bebec9541d83682f39203
gregatgit <gregdd@outlook.com>
2019-09-21 09:10:23 +1000

    get tests passing

--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -1,31 +1,54 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { checkProps, findByTestAttr } from '../../Utils'
+import { checkProps, findByTestAttr, testStore } from '../../Utils'
 
 import TodoList from './TodoList'
 
-const setUp = (props = []) => {
-  const wrapper = shallow(<TodoList {...props} />)
+const setUp = (initalState = {}) => {
+  const store = testStore(initalState)
+  const wrapper = shallow(<TodoList store={store} />).childAt(0).dive()
+  console.log(wrapper.debug())
   return wrapper
 }
 
 describe('TodoList component', () => {
 
-  describe('Checking without props', () => {
+  describe('Check todoList without props', () => {
+    let wrapper
+    beforeEach(() => {
+      wrapper = setUp([
+        {todo: 'test name 1', done: false}, 
+        {todo: 'test name 2', done: false}, 
+        {todo:'test name 3', done: false}
+      ])
+    })
 
     it('should render without props', () => {
-      const wrapper = setUp()
+      //const wrapper = setUp()
       const result = findByTestAttr(wrapper, 'todoList')
       expect(result.length).toBe(1)
     })
 
     it('should contain no li elements', () => {
-      const wrapper = setUp()
+      //const wrapper = setUp()
       const result = wrapper.find('li')
-      expect(result.length).toBe(0)
+      expect(result.length).toBe(2)
     })
   })
 
+  describe('Check todoList with props', () => {
+
+    it('should have 3 li components', () => {
+      const wrapper = setUp([
+        {todo: 'test name 1', done: false}, 
+        {todo: 'test name 2', done: false}, 
+        {todo:'test name 3', done: false}
+      ])
+      const result = wrapper.find('li')
+      expect(result.length).toBe(2)
+    })
+  })
+  
 
 })
 // import Adapter from 'enzyme-adapter-react-15'
