a9f204d281218a879dc5f5014eb5ffd87aa5da6c
gregatgit <gregdd@outlook.com>
2019-09-21 10:01:00 +1000

    create action delete and done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,20 @@
 export const ADD_TODO = 'ADD_TODO'
+export const DELETE_TODO = 'DELETE_TODO'
+export const TODO_DONE = 'TODO_DONE'
+
+export function todoDone(index) {
+  return {
+    type: TODO_DONE,
+    payload: index
+  }
+}
+
+export function deleteTodo(index) {
+  return {
+    type: DELETE_TODO,
+    payload: index
+  }
+}
 
 export function addTodo(name) {
   const newTodo = {todo: name, done: false}
